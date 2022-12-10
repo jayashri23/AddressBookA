da6bb37809447631c6443853cb5e1e490d091ffe
jayashri23 <jsjankar23@gmail.com>
2022-12-10 06:53:58 +0530

    Create Contact in Address Book

---
+++ src/com/addressbooka/AddressBookMain.java
@@ -0,0 +1,9 @@
+package com.addressbooka;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        AddressBook obj =new AddressBook("Jaya","Vadde","Chembure","Mumbai",
+                "Maharashtra",987654432,"jsjankar23@gmail.com");
+        System.out.println(obj);
+    }
+}
